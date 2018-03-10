ddbfe1aaf264dc714f723b1b85a506878c948622
Rafael Possenti <rafael_possenti@hotmail.com>
2018-03-10 19:03:50 -0200

    one to one mapping
    Change-Id: I90d031ca04d41331892619057a767463afe4112e
    Signed-off-by: Rafael Possenti <rafael_possenti@hotmail.com>

---
+++ src/com/luv2code/springdemo/mvc/CreateDemo.java
@@ -0,0 +1,53 @@
+package com.luv2code.springdemo.mvc;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.springdemo.entity.Instructor;
+import com.luv2code.springdemo.entity.InstructorDetail;
+import com.luv2code.springdemo.entity.Student;
+
+public class CreateDemo {
+	public static void main(String Args[]) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		
+		//create session 
+		Session session = factory.getCurrentSession();
+		
+		try {
+			//create the objects
+			Instructor tempInstructor = new Instructor("Chad","Darby","darby@hotmail.com");
+			
+			InstructorDetail tempInstructorDetail = new InstructorDetail("https://www.youtube.com/user/luv2codetv",
+					"Luv 2 code!!");
+			
+			//associate the objects
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
+			
+			// start a transaction 
+			session.beginTransaction();
+			
+			//NOTE: this will ALSO save the details object
+			//beacause of CascadeType.ALL
+			//save the instructor
+			System.out.println("saving instructor" + tempInstructor);
+			session.save(tempInstructor);
+			
+			
+			//commit transaction
+			session.getTransaction().commit();
+			
+			
+		}finally {
+			factory.close();
+		}
+		
+	}	
+}
